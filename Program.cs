8507a58b3269d78efdd529b758b549bedf0976b9
camilocro <camilorodriguezandre@gmail.com>
2025-09-26 10:28:09 -0400

    Cors Implementation and Limiters

--- Program.cs
+++ Program.cs
@@ -1,4 +1,25 @@
+
+using Microsoft.AspNetCore.RateLimiting;
+using System.Threading.RateLimiting;
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+});
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddFixedWindowLimiter("Fixed", opt =>
+    {
+        opt.PermitLimit = 10;
+        opt.Window = TimeSpan.FromSeconds(10);
+        opt.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+        opt.QueueLimit = 2;
+    });
+});
 
 // Add services to the container.
 
@@ -8,10 +29,15 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+app.UseRateLimiter();
+app.UseCors("AllowAll");
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapControllers().RequireRateLimiting("Fixed");
 
 app.Run();
