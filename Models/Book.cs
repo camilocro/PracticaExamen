a56da34cfe0469b4c8fa9fd9b087dd1143c0616c
camilocro <camilorodriguezandre@gmail.com>
2025-09-26 09:35:36 -0400

    Fixed

---
+++ Models/Book.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+public class Book
+{
+    public Guid Id { get; set; }
+    [Required, StringLength(150)]
+    public string Title { get; set; }
+
+    [Required, StringLength(100)]
+    public string Author { get; set; }
+    [Required, StringLength(50)]
+    public string Genre { get; set; }
+    [Range(1800,2025)]
+    public int PublicationYear { get; set; }
+}
+    
+public record CreteBookDto
+{
+    [Required, StringLength(150)]
+    public string Title { get; set; }
+
+    [Required, StringLength(100)]
+    public string Author { get; set; }
+    [Required, StringLength(50)]
+    public string Genre { get; set; }
+    [Range(1800, 2025)]
+    public int PublicationYear { get; set; }
+}
+
+public record UpdateBookDto
+{
+    [Required, StringLength(150)]
+    public string Title { get; set; }
+
+    [Required, StringLength(100)]
+    public string Author { get; set; }
+    [Required, StringLength(50)]
+    public string Genre { get; set; }
+    [Range(1800, 2025)]
+    public int PublicationYear { get; set; }
+}
--- obj/Debug/net9.0/TecWeb Practica Examen.AssemblyInfo.cs
+++ obj/Debug/net9.0/TecWeb Practica Examen.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TecWeb Practica Examen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7850cb68328821cdccd2651b09131ffb8ba994f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TecWeb Practica Examen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TecWeb Practica Examen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
